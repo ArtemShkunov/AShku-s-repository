027349ac655cbaa7adee3523ca910b39416305a0
shayu25u578 <shkunovayu@student.bmstu.ru>
2026-07-11 01:15:05 +0300

    Try fix treesitter.

--- NixOS/nvim/lua/plugins/treesitter.lua
+++ NixOS/nvim/lua/plugins/treesitter.lua
@@ -3,46 +3,16 @@ return {
     "nvim-treesitter",
     dir = plugin_path("nvim-treesitter"),
     name = "nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    dependencies = {
-      {
-        "nvim-treesitter-textobjects",
-        dir = plugin_path("nvim-treesitter-textobjects"),
-        name = "nvim-treesitter-textobjects",
-      },
-    },
+    lazy = false,
     config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {}, -- парсеры уже ставятся через Nix, доп. установка не нужна
-        auto_install = false,  -- важно: не пытаться скачивать через :TSInstall
-        highlight = { enable = true },
-        indent = { enable = true },
-
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true,
-            goto_next_start = {
-              ["]f"] = "@function.outer",
-              ["]c"] = "@class.outer",
-            },
-            goto_previous_start = {
-              ["[f"] = "@function.outer",
-              ["[c"] = "@class.outer",
-            },
-          },
-        },
+      -- Новый API: парсеры уже установлены через Nix, TSInstall не нужен.
+      -- Просто включаем подсветку и отступы через нативный vim.treesitter.
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "c", "cpp", "python", "lua", "nix", "bash", "markdown", "vim" },
+        callback = function(args)
+          vim.treesitter.start(args.buf)
+          vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
       })
     end,
   },
