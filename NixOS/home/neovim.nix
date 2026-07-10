1067b6500926accfffdda818742019468c0523b1
shayu25u578 <shkunovayu@student.bmstu.ru>
2026-07-10 23:24:03 +0300

    Revert "Add some more nvim plugins."
    This reverts commit 20e9a67540fbdf70a44364bef8129dbc290705f1.

--- NixOS/home/neovim.nix
+++ NixOS/home/neovim.nix
@@ -30,10 +30,6 @@ let
     nvim-surround = nvim-surround;
     indent-blankline-nvim = indent-blankline-nvim;
     comment-nvim = comment-nvim;
-
-    nvim-treesitter = nvim-treesitter;
-    nvim-treesitter-textobjects = nvim-treesitter-textobjects;
-    noice-nvim = noice-nvim;
   };
 
   # Генерируем Lua-таблицу вида:
