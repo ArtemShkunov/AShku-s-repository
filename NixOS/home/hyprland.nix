3a997e5245a7a3ccf0025a7eac6d226a21496d64
shayu25u578 <shkunovayu@student.bmstu.ru>
2026-07-12 21:40:13 +0300

    Hopefully get hyprpaper to work(attempt 4).

--- NixOS/home/hyprland.nix
+++ NixOS/home/hyprland.nix
@@ -113,7 +113,7 @@ in
   # Замени путь на свою картинку.
   xdg.configFile."hypr/hyprpaper.conf".text = ''
     preload=${config.home.homeDirectory}/Data/Wallpaper.jpg
-    wallpaper=${config.home.homeDirectory}/Data/Wallpaper.jpg
+    wallpaper=eDp-1,${config.home.homeDirectory}/Data/Wallpaper.jpg
     splash = false
   '';
 
