891000d077d714354041a0ec1cc7b0fb12a49d53
shayu25u578 <shkunovayu@student.bmstu.ru>
2026-07-12 11:38:20 +0300

    Fix some small issies with hyprland(still attempt 3).

--- NixOS/home/hyprland.nix
+++ NixOS/home/hyprland.nix
@@ -111,8 +111,8 @@ in
   # напрямую как обычный текстовый файл — так надёжнее.
   # Замени путь на свою картинку.
   xdg.configFile."hypr/hyprpaper.conf".text = ''
-    preload = ${config.home.homeDirectory}/Data/Wallpaper.png
-    wallpaper = ,${config.home.homeDirectory}/Data/Wallpaper.png
+    preload = ${config.home.homeDirectory}/Data/Wallpaper.jpg
+    wallpaper = ,${config.home.homeDirectory}/Data/Wallpaper.jpg
     splash = false
   '';
 
