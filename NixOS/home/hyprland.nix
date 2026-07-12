4a72306f2130328a868ffb69b61eedb9dd840b3b
shayu25u578 <shkunovayu@student.bmstu.ru>
2026-07-12 22:23:39 +0300

    Hopefully get hyprpaper to work(attempt 5).

--- NixOS/home/hyprland.nix
+++ NixOS/home/hyprland.nix
@@ -23,7 +23,7 @@ in
       exec-once = [
         "waybar"
         "hypridle"
-        "hyprpaper"
+        "sh -c 'pkill hyprpaper; sleep 1; hyprpaper'"
         "nm-applet --indicator"
         "wl-paste --watch cliphist store"
         "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"
@@ -113,7 +113,7 @@ in
   # Замени путь на свою картинку.
   xdg.configFile."hypr/hyprpaper.conf".text = ''
     preload=${config.home.homeDirectory}/Data/Wallpaper.jpg
-    wallpaper=eDp-1,${config.home.homeDirectory}/Data/Wallpaper.jpg
+    wallpaper=,${config.home.homeDirectory}/Data/Wallpaper.jpg
     splash = false
   '';
 
