c0cac45bc112f3ac38bf5b67c26cbf6ef0de40f6
shayu25u578 <shkunovayu@student.bmstu.ru>
2026-07-17 00:37:17 +0300

    Try disaple xfce(1).

--- NixOS/hosts/honor-magicbook-x16-plus/configuration.nix
+++ NixOS/hosts/honor-magicbook-x16-plus/configuration.nix
@@ -51,7 +51,7 @@
   # services.xserver.desktopManager.xfce.enable = true;
   # Отдельно включить thunar
   services.gvfs.enable = true; # Удаленный доступ к файлам и корзина
-  services.tumblr.enable = true; # Генерация миниатюр (картинок) для Thunar
+  services.tumbler.enable = true; # Генерация миниатюр (картинок) для Thunar
   programs.thunar.enable = true; # Сам Thunar + интеграция в систему
   programs.thunar.plugins = with pkgs.xfce; [
     thunar-archive-plugin # Интеграция с архиватором (у вас стоит Ark)
