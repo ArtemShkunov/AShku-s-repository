aa7a6bf7e0115a08514f41f36ac2ae01f0d297bb
shayu25u578 <shkunovayu@student.bmstu.ru>
2026-07-20 11:43:47 +0300

    add bluetooth.

--- NixOS/hosts/honor-magicbook-x16-plus/configuration.nix
+++ NixOS/hosts/honor-magicbook-x16-plus/configuration.nix
@@ -203,6 +203,18 @@
     };
   };
 
+
+  # Enable Bluetooth
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+      };
+    };
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
