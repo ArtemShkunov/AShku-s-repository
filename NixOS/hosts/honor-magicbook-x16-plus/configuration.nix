ef0de7b164290aefe6d274c45d5c02d8a9a07873
shayu25u578 <shkunovayu@student.bmstu.ru>
2026-08-16 21:49:49 +0300

    add tui managers(2).

--- NixOS/hosts/honor-magicbook-x16-plus/configuration.nix
+++ NixOS/hosts/honor-magicbook-x16-plus/configuration.nix
@@ -5,227 +5,228 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./greeter.nix
+    imports =
+        [ # Include the results of the hardware scan.
+            ./hardware-configuration.nix
+            ./greeter.nix
+        ];
+
+    # Bootloader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    networking.hostName = "honor-magicbook-x16-plus"; # Define your hostname.
+    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+    # Enable networking
+    networking.networkmanager.enable = true;
+    networking.wireless.iwd.enable = true;
+
+    # Set your time zone.
+    time.timeZone = "Europe/Moscow";
+
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_US.UTF-8";
+
+    i18n.extraLocaleSettings = {
+        LC_ADDRESS = "ru_RU.UTF-8";
+        LC_IDENTIFICATION = "ru_RU.UTF-8";
+        LC_MEASUREMENT = "ru_RU.UTF-8";
+        LC_MONETARY = "ru_RU.UTF-8";
+        LC_NAME = "ru_RU.UTF-8";
+        LC_NUMERIC = "ru_RU.UTF-8";
+        LC_PAPER = "ru_RU.UTF-8";
+        LC_TELEPHONE = "ru_RU.UTF-8";
+        LC_TIME = "ru_RU.UTF-8";
+    };
+
+    # Enable the X11 windowing system.
+    services.xserver.enable = true;
+
+    # Enable the XFCE Desktop Environment.
+    # services.xserver.displayManager.lightdm.enable = true;
+    # services.xserver.desktopManager.xfce.enable = true;
+    # Отдельно включить thunar
+    services.gvfs.enable = true; # Удаленный доступ к файлам и корзина
+    services.tumbler.enable = true; # Генерация миниатюр (картинок) для Thunar
+    programs.thunar.enable = true; # Сам Thunar + интеграция в систему
+    programs.thunar.plugins = with pkgs; [
+        thunar-archive-plugin # Интеграция с архиватором (у вас стоит Ark)
+        thunar-volman # Автомонтирование флешек
+    ];
+    # Enable Hyprland
+    programs.hyprland.enable = true;
+    #xdg.portal = {
+    #  enable = true;
+    #  extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    #  config = {
+    #    common.default = [ "gtk" ];
+    #    hyprland.default = [ "hyprland" "gtk" ];
+    #  };
+    #};
+    # Enable dconf
+    programs.dconf.enable = true;
+    security.pam.services.hyprlock = {};
+
+    # Демон polkit. networkmanager обычно включает его сам через mkDefault,
+    # но полагаться на неявное поведение другого модуля — плохая идея:
+    # без него hyprpolkitagent просто не будет к чему подключаться.
+    security.polkit.enable = true;
+
+
+    # Configure keymap in X11
+    services.xserver.xkb = {
+        layout = "us";
+        variant = "";
+    };
+
+    # Enable flakes
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+    # Enable CUPS to print documents.
+    services.printing.enable = true;
+
+    # Enable sound with pipewire.
+    services.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        # If you want to use JACK applications, uncomment this
+        #jack.enable = true;
+
+        # use the example session manager (no others are packaged yet so this is enabled by default,
+        # no need to redefine it in your config for now)
+        #media-session.enable = true;
+    };
+
+    # Enable touchpad support (enabled default in most desktopManager).
+    # services.xserver.libinput.enable = true;
+
+
+    # Allow zsh
+    programs.zsh.enable = true;
+
+
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users."artemmkk-sh" = {
+        isNormalUser = true;
+        description = "Artemmkk-sh";
+        extraGroups = [ "networkmanager" "wheel" ];
+        shell = pkgs.zsh;
+    };
+
+    # Enable throne
+    programs.throne.enable = true;
+    programs.throne.tunMode.enable = true;
+
+    # Install firefox.
+    programs.firefox.enable = true;
+
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
+
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [
+        vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+        wget
+        gparted
+        ntfs3g
+        kdePackages.ark # for archieves
+        neovim # maybe someday i will turn it into an IDE
+        git # need for flakes and many many other things
+        kitty
+        gnome-themes-extra
+        adwaita-icon-theme
+        adwaita-qt
+        kdePackages.qtwayland
+        # Пакеты из xfce
+        mousepad
+        ristretto
+
+        # polkit_gnome
     ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+    # Fonts configuration
 
-  networking.hostName = "honor-magicbook-x16-plus"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Europe/Moscow";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "ru_RU.UTF-8";
-    LC_IDENTIFICATION = "ru_RU.UTF-8";
-    LC_MEASUREMENT = "ru_RU.UTF-8";
-    LC_MONETARY = "ru_RU.UTF-8";
-    LC_NAME = "ru_RU.UTF-8";
-    LC_NUMERIC = "ru_RU.UTF-8";
-    LC_PAPER = "ru_RU.UTF-8";
-    LC_TELEPHONE = "ru_RU.UTF-8";
-    LC_TIME = "ru_RU.UTF-8";
-  };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the XFCE Desktop Environment.
-  # services.xserver.displayManager.lightdm.enable = true;
-  # services.xserver.desktopManager.xfce.enable = true;
-  # Отдельно включить thunar
-  services.gvfs.enable = true; # Удаленный доступ к файлам и корзина
-  services.tumbler.enable = true; # Генерация миниатюр (картинок) для Thunar
-  programs.thunar.enable = true; # Сам Thunar + интеграция в систему
-  programs.thunar.plugins = with pkgs; [
-    thunar-archive-plugin # Интеграция с архиватором (у вас стоит Ark)
-    thunar-volman # Автомонтирование флешек
-  ];
-  # Enable Hyprland
-  programs.hyprland.enable = true;
-  #xdg.portal = {
-  #  enable = true;
-  #  extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-  #  config = {
-  #    common.default = [ "gtk" ];
-  #    hyprland.default = [ "hyprland" "gtk" ];
-  #  };
-  #};
-  # Enable dconf
-  programs.dconf.enable = true;
-  security.pam.services.hyprlock = {};
-
-  # Демон polkit. networkmanager обычно включает его сам через mkDefault,
-  # но полагаться на неявное поведение другого модуля — плохая идея:
-  # без него hyprpolkitagent просто не будет к чему подключаться.
-  security.polkit.enable = true;
+    fonts.packages = with pkgs; [
+        noto-fonts
+        noto-fonts-cjk-sans
+        noto-fonts-cjk-serif
+        noto-fonts-color-emoji
+        fira
+        fira-math
+        nerd-fonts.fira-code
+        nerd-fonts.jetbrains-mono
+        corefonts
+    ];
 
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Enable flakes
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-  
-
-  # Allow zsh
-  programs.zsh.enable = true;
-
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users."artemmkk-sh" = {
-    isNormalUser = true;
-    description = "Artemmkk-sh";
-    extraGroups = [ "networkmanager" "wheel" ];
-    shell = pkgs.zsh;
-  };
-
-  # Enable throne
-  programs.throne.enable = true;
-  programs.throne.tunMode.enable = true;
-
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    gparted
-    ntfs3g
-    kdePackages.ark # for archieves
-    neovim # maybe someday i will turn it into an IDE
-    git # need for flakes and many many other things
-    kitty
-    gnome-themes-extra
-    adwaita-icon-theme
-    adwaita-qt
-    kdePackages.qtwayland
-    # Пакеты из xfce
-    mousepad
-    ristretto
-
-    # polkit_gnome
-  ];
-
-  # Fonts configuration
-
-  fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-cjk-serif
-    noto-fonts-color-emoji
-    fira
-    fira-math
-    nerd-fonts.fira-code
-    nerd-fonts.jetbrains-mono
-    corefonts
-  ];
-
-
-  environment.variables = {
-    TERMINAL = "kitty";
-  };
-
-
-  boot.supportedFilesystems = [ "ntfs" "exfat" ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = true;
-  
-
-
-
-  # Enable battery control
-  services.tlp = {
-    enable = true;
-    settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      START_CHARGE_THRESH_BAT0 = 75;
-      STOP_CHARGE_THRESH_BAT0 = 80;
+    environment.variables = {
+        TERMINAL = "kitty";
     };
-  };
 
 
-  # Enable Bluetooth
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-    settings = {
-      General = {
-        Experimental = true;
-      };
+    boot.supportedFilesystems = [ "ntfs" "exfat" ];
+
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # programs.mtr.enable = true;
+    # programs.gnupg.agent = {
+    #   enable = true;
+    #   enableSSHSupport = true;
+    # };
+
+    # List services that you want to enable:
+
+    # Enable the OpenSSH daemon.
+    # services.openssh.enable = true;
+
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    networking.firewall.enable = true;
+
+
+
+
+    # Enable battery control
+    services.tlp = {
+        enable = true;
+        settings = {
+            CPU_SCALING_GOVERNOR_ON_AC = "performance";
+            CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+            START_CHARGE_THRESH_BAT0 = 75;
+            STOP_CHARGE_THRESH_BAT0 = 80;
+        };
     };
-  };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "26.05"; # Did you read the comment?
+
+    # Enable Bluetooth
+    hardware.bluetooth = {
+        enable = true;
+        powerOnBoot = true;
+        settings = {
+            General = {
+                Experimental = true;
+            };
+        };
+    };
+
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "26.05"; # Did you read the comment?
 
 }
