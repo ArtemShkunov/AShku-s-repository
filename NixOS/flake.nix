333438fa531555c99746aa9fc276775c0a7e3372
shayu25u578 <shkunovayu@student.bmstu.ru>
2026-08-18 23:14:42 +0300

    comment tmux and add zellij(3).

--- NixOS/flake.nix
+++ NixOS/flake.nix
@@ -1,39 +1,41 @@
 {
-  description = "NixOS configuration";
+    description = "NixOS configuration";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    home-manager = {
-      url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "nixpkgs";
+        home-manager = {
+            url = "github:nix-community/home-manager/master";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+
+        zen-browser = {
+            url = "github:0xc000022070/zen-browser-flake";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
 
-    zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
+    outputs = { self, nixpkgs, home-manager, zen-browser,  ... }@inputs:
+        let
+            system = "x86_64-linux";
+        in
+            {
+            nixosConfigurations."honor-magicbook-x16-plus" = nixpkgs.lib.nixosSystem {
+                inherit system;
+                specialArgs = { inherit inputs; };
+                modules = [
+                    ./hosts/honor-magicbook-x16-plus/configuration.nix
 
-  outputs = { self, nixpkgs, home-manager, zen-browser,  ... }@inputs:
-    let
-      system = "x86_64-linux";
-    in
-      {
-      nixosConfigurations."honor-magicbook-x16-plus" = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./hosts/honor-magicbook-x16-plus/configuration.nix
+                    home-manager.nixosModules.home-manager
+                    {
+                        home-manager.useGlobalPkgs = true;
+                        home-manager.backupFileExtension = "backup";
+                        home-manager.useUserPackages = true;
+                        home-manager.extraSpecialArgs = { inherit inputs; };
+                        home-manager.users."artemmkk-sh" = import ./home/artemmkk-sh.nix;
 
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit inputs; };
-            home-manager.users."artemmkk-sh" = import ./home/artemmkk-sh.nix;
-          } 
-        ];
-      };
-    };
+                    } 
+                ];
+            };
+        };
 }
