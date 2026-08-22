50f2c347fa928da97cb2fafd59f63c1bbccd2fe4
shayu25u578 <shkunovayu@student.bmstu.ru>
2026-08-22 15:28:13 +0300

    try add nvidia gpu drivers

--- NixOS/hosts/Desktop/device.nix
+++ NixOS/hosts/Desktop/device.nix
@@ -10,4 +10,25 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   boot.supportedFilesystems = [ "ntfs" "exfat" ];
+
+  # Dual GPU: Intel iGPU + NVIDIA RTX 3050
+  # NOTE: For PRIME offload, run `lspci -nn | grep -E 'VGA|3D'` on the machine
+  # and set hardware.nvidia.prime.offload.intelBusId / amdgpuBusId accordingly
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = false;
+    powerManagement.enable = true;
+    powerManagement.finegrained = false;
+  };
+
+  # Intel iGPU
+  hardware.opengl = {
+    enable = true;
+    driSupport32Bit = true;
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" "intel" ];
+
+  boot.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" "i915" ];
+  boot.initrd.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" "i915" ];
 }
--- NixOS/hosts/Desktop/home.nix
+++ NixOS/hosts/Desktop/home.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
-  wayland.windowsManager.hyprland.settings.monitor = {
+  wayland.windowManager.hyprland.settings.monitor = {
     output = "DP-1";
     mode = "1920x1080@120";
     position = "auto";
@@ -8,11 +8,20 @@
 
   programs.waybar.settings.mainBar."hyprland/language".keyboard-name = "company--usb-device--keyboard";
 
+  # Dual GPU: NVIDIA RTX 3050 + Intel iGPU
+  wayland.windowManager.hyprland.settings.env = [
+    { _args = [ "WLR_NO_HARDWARE_CURSORS" "1" ]; }
+    { _args = [ "WLR_RENDERER_ALLOW_SOFTWARE" "1" ]; }
+    { _args = [ "NVD_BACKEND" "direct" ]; }
+    { _args = [ "__GL_GSYNC_ALLOWED" "0" ]; }
+    { _args = [ "__GL_VRR_ALLOWED" "0" ]; }
+    { _args = [ "__NV_PRIME_RENDER_OFFLOAD" "1" ]; }
+    { _args = [ "__GLX_VENDOR_LIBRARY_NAME" "nvidia" ]; }
+    { _args = [ "__VK_LAYER_NV_optimus" "NVIDIA_only" ]; }
+  ];
+
   home.packages = with pkgs; [
     logiops
     logitech-udev-rules
   ];
-
-
-
 }
